c4f1bae0b128727b2680d8fc3cf1d4b45763b44a
Nick Void <mn3m00@gmail.com>
2017-12-05 16:55:44 +0300

    updated format using "go fmt"

--- task1/execution_test.go
+++ task1/execution_test.go
@@ -1,22 +1,24 @@
 package main
+
 import (
-  "fmt"
-  "testing"
-  "time"
+	"fmt"
+	"testing"
+	"time"
 )
+
 // Testing only by no exception for now
 // * TODO: Add more complex checks
-func Test_f1 (t *testing.T) {
-  fmt.Println(f(77))
+func Test_f1(t *testing.T) {
+	fmt.Println(f(77))
 }
-func Test_f2 (t *testing.T) {
-  fmt.Println(f(77,321))
+func Test_f2(t *testing.T) {
+	fmt.Println(f(77, 321))
 }
-func Test_f3 (t *testing.T) {
-  tmp, _ := time.Parse("2006-Jan-02", "2013-Feb-03")
-  fmt.Println(f(77,321, tmp ))
+func Test_f3(t *testing.T) {
+	tmp, _ := time.Parse("2006-Jan-02", "2013-Feb-03")
+	fmt.Println(f(77, 321, tmp))
 }
-func Test_f4 (t *testing.T) {
-  tmp, _ := time.Parse("2006-Jan-02", "2013-Feb-03")
-  fmt.Println(f(77,321, tmp,  &point{10,50,255}))
+func Test_f4(t *testing.T) {
+	tmp, _ := time.Parse("2006-Jan-02", "2013-Feb-03")
+	fmt.Println(f(77, 321, tmp, &point{10, 50, 255}))
 }
--- task1/main.go
+++ task1/main.go
@@ -1,51 +1,54 @@
 package main
-import ( 
-  "fmt"
-  "time"
+
+import (
+	"fmt"
+	"time"
 )
+
 type point struct {
-  x int
-  y int
-  value int
+	x     int
+	y     int
+	value int
 }
 type fout struct {
-  n1,n2 int
-  t time.Time
-  p *point
+	n1, n2 int
+	t      time.Time
+	p      *point
 }
-func f(in1 int, args ...interface{}) (o fout)   {
-  // setting default values:
-  o.n1=in1
-  o.n2=10
-  o.t=time.Now()
-  o.p=nil
-  // parsing arguments
-  l:=len(args)
-  if l>0 {
-    o.n2=args[0].(int)
-  }
-  if l>1 {
-    o.t=args[1].(time.Time)
-  }
-  if l>2 {
-    o.p=args[2].(*point)
-  }
-  return
+
+func f(in1 int, args ...interface{}) (o fout) {
+	// setting default values:
+	o.n1 = in1
+	o.n2 = 10
+	o.t = time.Now()
+	o.p = nil
+	// parsing arguments
+	l := len(args)
+	if l > 0 {
+		o.n2 = args[0].(int)
+	}
+	if l > 1 {
+		o.t = args[1].(time.Time)
+	}
+	if l > 2 {
+		o.p = args[2].(*point)
+	}
+	return
 }
 func main() {
-  tmp, _ := time.Parse("2006-Jan-02", "2013-Feb-03")
-  //fmt.Println(f())
-  // Not allowed: not enough arguments in call to f
-  fmt.Println(f(77).n1)
-  // Output: 77
-  fmt.Println(f(77,321, tmp, &point{10,20,255}).n1)
-  // Output: 77
-  fmt.Println(f(77).n2)
-  // Output: 10 -- default value
-  fmt.Println(f(77,321, tmp, &point{10,20,255}).n2)
-  // Output: 321
-  fmt.Println(f(77,321, tmp, &point{10,20,255}).t)
-  // Output: 2013-02-03 00:00:00 +0000 UTC
-  fmt.Println(f(77,321).t)
-  // Output: now
+	tmp, _ := time.Parse("2006-Jan-02", "2013-Feb-03")
+	//fmt.Println(f())
+	// Not allowed: not enough arguments in call to f
+	fmt.Println(f(77).n1)
+	// Output: 77
+	fmt.Println(f(77, 321, tmp, &point{10, 20, 255}).n1)
+	// Output: 77
+	fmt.Println(f(77).n2)
+	// Output: 10 -- default value
+	fmt.Println(f(77, 321, tmp, &point{10, 20, 255}).n2)
+	// Output: 321
+	fmt.Println(f(77, 321, tmp, &point{10, 20, 255}).t)
+	// Output: 2013-02-03 00:00:00 +0000 UTC
+	fmt.Println(f(77, 321).t)
+	// Output: now
 }
